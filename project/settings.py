ac5eaddcf91c837fa32ec7e71a60aed42689fca6
willanderdev <willanderdev@gmail.com>
2022-10-02 20:41:59 -0300

    correção

--- project/settings.py
+++ project/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 # STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_URL = '/static/'
+MEDIA_URL = '/staticfiles/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 django_heroku.settings(locals())
 
