76b7654507f781ab382e44f271915517f0a2e89e
willanderdev <willanderdev@gmail.com>
2022-10-02 22:07:20 -0300

    correção

--- project/settings.py
+++ project/settings.py
@@ -147,8 +147,10 @@ USE_TZ = True
 # STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_URL = '/staticfiles/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_URL = '/staticfiles/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'setup/static'),
+]
 django_heroku.settings(locals())
 
 # Default primary key field type
